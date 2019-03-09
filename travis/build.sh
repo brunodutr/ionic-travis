0dd9425123632c69fe10ed8a6964d081525624ab
Bruno Dutra <brun_dutra@hotmail.com>
2019-03-09 18:28:02 -0300

    Correção: Configuracao de versionamento 9

--- travis/build.sh
+++ travis/build.sh
@@ -7,13 +7,9 @@ npm run lint
 if [[ "$TRAVIS_BRANCH" == "prod" ]]
 then
     echo "Build APK para Produção"
-    ionic cordova build android --prod
+    ionic cordova build android --prod --release
     git checkout -- .
-    export VERSION=$(npm version patch)
-    export TRAVIS_TAG=${TRAVIS_TAG:-$(v${VERSION})}
 else
     echo "Build APK para Desenvolvimento"
     ionic cordova build android
-    export VERSION=$(node -p -e "require('./package.json').version")
-    export TRAVIS_TAG=${TRAVIS_TAG:-$(v${VERSION}-SNAPSHOT)}
 fi

d83b2a83d1505e0ea7146b313f52203d562cc608
Bruno Dutra <brun_dutra@hotmail.com>
2019-03-09 16:27:19 -0300

    Configuracao de versionamento

--- travis/build.sh
+++ travis/build.sh
@@ -8,7 +8,11 @@ if [[ "$TRAVIS_BRANCH" == "prod" ]]
 then
     echo "Build APK para Produção"
     ionic cordova build android --prod --release
+    export VERSION = $(npm version patch)
+    export TRAVIS_TAG= $('V' + $VERSION)
 else
     echo "Build APK para Desenvolvimento"
     ionic cordova build android
+    export VERSION = $(node -p -e "require('./package.json').version")
+    export TRAVIS_TAG= $('V' + $VERSION + '-SNAPSHOT')
 fi

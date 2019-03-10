80bd2e18f94c1afd2b27c2b6ed1821225b912382
Bruno Dutra <brun_dutra@hotmail.com>
2019-03-10 03:30:40 -0300

    corrigir bash release.sh

--- travis/release.sh
+++ travis/release.sh
@@ -2,6 +2,7 @@ set -e
 
 npm version patch --no-git-tag-version
 
+git checkout $TRAVIS_BRANCH
 git add .
 git commit -m "[skip travis] PREPARANDO PROJETO PARA A PRÓXIMA VERSÃO: $(node -p -e "require('./package.json').version")"
 git push https://${GITHUB_TOKEN}@github.com/${TRAVIS_REPO_SLUG} prod:master

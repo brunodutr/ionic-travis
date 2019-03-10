5fdde0e992f1548652a51b28946583188e05a41f
Bruno Dutra <brun_dutra@hotmail.com>
2019-03-10 02:24:47 -0300

    Corrigindo BASH release.sh 2

--- travis/release.sh
+++ travis/release.sh
@@ -3,6 +3,6 @@ set -e
 
 git add .
 git commit -m "[skip travis] PREPARANDO PROJETO PARA A PRÓXIMA VERSÃO: $(npm version patch --no-git-tag-version)"
-git push https://github.com/${TRAVIS_REPO_SLUG} master
+git push https://github.com/${TRAVIS_REPO_SLUG} prod:master
 
 

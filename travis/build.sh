87319f6ff5fded3e269791f89f9ee290c368e824
Bruno Dutra <brun_dutra@hotmail.com>
2019-03-10 18:37:31 -0300

    Corrigindo assinatura de aplicativo

--- travis/build.sh
+++ travis/build.sh
@@ -13,6 +13,8 @@ then
     cd platforms/android/app/build/outputs/apk/release
     jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -storepass $KEYSTORE_PASSWORD -keystore /home/travis/build/$TRAVIS_REPO_SLUG/ionictravis.keystore app-release-unsigned.apk ionic_travis_key
     ${ANDROID_HOME}/build-tools/27.0.3/zipalign -v 4 app-release-unsigned.apk app-release.apk
+    ls
+    rm app-release-unsigned.apk
 else
     echo "Build APK para Desenvolvimento"
     ionic cordova build android

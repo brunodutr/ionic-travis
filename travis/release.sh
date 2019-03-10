5286b1a860db3ea97444590d78a0b85d19253718
Bruno Dutra <brun_dutra@hotmail.com>
2019-03-10 02:22:35 -0300

    Corrigindo BASH release.sh

--- travis/release.sh
+++ travis/release.sh
@@ -2,9 +2,7 @@ set -e
 
 
 git add .
-git commit -am "[skip travis] PREPARANDO PROJETO PARA A PRÓXIMA VERSÃO: $(npm version patch --no-git-tag-version)"
-git remote rm origin
-git remote add origin https://github.com/${TRAVIS_REPO_SLUG};
-git push origin master
+git commit -m "[skip travis] PREPARANDO PROJETO PARA A PRÓXIMA VERSÃO: $(npm version patch --no-git-tag-version)"
+git push https://github.com/${TRAVIS_REPO_SLUG} master
 
 

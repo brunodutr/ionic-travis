35b1f1178a962bf84f2181214895dc41a54bbe5a
Bruno Dutra <brun_dutra@hotmail.com>
2019-03-10 18:51:04 -0300

    Alterações no release.sh

--- travis/release.sh
+++ travis/release.sh
@@ -5,10 +5,10 @@ then
     echo "Versionamento o aplicativo"
     npm version patch --no-git-tag-version
 
-    git checkout master
+    git checkout prod
     git add .
     git commit -m "[skip travis] PREPARANDO PROJETO PARA A PRÓXIMA VERSÃO: $(node -p -e "require('./package.json').version")"
-    git push https://${GITHUB_TOKEN}@github.com/${TRAVIS_REPO_SLUG} master
+    git push https://${GITHUB_TOKEN}@github.com/${TRAVIS_REPO_SLUG} prod:master
 
 fi
 

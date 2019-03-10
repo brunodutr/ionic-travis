3f12d1c18e8afbdc946c7e8e9a32f666a7911e7d
Bruno Dutra <brun_dutra@hotmail.com>
2019-03-10 18:13:56 -0300

    Corrigit caminho para a pasta de Releases

--- travis/build.sh
+++ travis/build.sh
@@ -10,7 +10,7 @@ then
     ionic cordova build android --prod --release
     
     echo "Sign APK para Produção"
-    cd platforms/android/app/build/outputs/apk
+    cd platforms/android/app/build/outputs/apk/release
     jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -storepass $KEYSTORE_PASSWORD -keystore /home/travis/build/$TRAVIS_REPO_SLUG/ionictravis.keystore app-release-unsigned.apk ionic_travis_key
     zipalign -v 4 app-release-unsigned.apk app-release.apk
 else

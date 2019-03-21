0bddcaa2e9723cc5724b7ef402e26d61d3926ad9
Bruno Dutra <brun_dutra@hotmail.com>
2019-03-21 00:43:11 -0300

    Testar o uso de jobs

--- travis/release.sh
+++ travis/release.sh
@@ -1,19 +1,12 @@
 set -e 
 
+echo "BRANCH: $TRAVIS_BRANCH | RELEASE"
 
-if [[ "$TRAVIS_BRANCH" == "prod" ]]
-then
+rm ionictravis-${VERSION}.apk
 
-    echo "BRANCH: $TRAVIS_BRANCH | RELEASE"
+npm version patch --no-git-tag-version
 
-    rm ionictravis-${VERSION}.apk
-    
-    npm version patch --no-git-tag-version
-
-    git checkout prod
-    git add .
-    git commit -m "[skip travis] PREPARANDO PROJETO PARA A PRÓXIMA VERSÃO: $(node -p -e "require('./package.json').version")"
-    git push https://${GITHUB_TOKEN}@github.com/${TRAVIS_REPO_SLUG} prod:master
-
-fi
-
+git checkout prod
+git add .
+git commit -m "[skip travis] PREPARANDO PROJETO PARA A PRÓXIMA VERSÃO: $(node -p -e "require('./package.json').version")"
+git push https://${GITHUB_TOKEN}@github.com/${TRAVIS_REPO_SLUG} prod:master

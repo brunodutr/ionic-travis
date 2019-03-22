f8f16482a4f0910fcfa4817df2a89a57cfa063c4
Bruno Dutra <brun_dutra@hotmail.com>
2019-03-21 22:36:01 -0300

    Configurar .travis.yml

--- travis/before_deploy.sh
+++ travis/before_deploy.sh
@@ -2,7 +2,7 @@ set -e
 
 echo "BRANCH: $TRAVIS_BRANCH | PREPARE DEPLOY"
 
-ln -s /home/travis/build/$TRAVIS_REPO_SLUG/platforms/android/app/build/outputs/apk/*/*.apk ionictravis-${VERSION}.apk
+ln -s ~/$TRAVIS_BUILD_NUMBER/*.apk ionictravis-${VERSION}.apk
 git tag -f v${VERSION}
 git remote add travis https://${GITHUB_TOKEN}@github.com/${TRAVIS_REPO_SLUG}
 git push travis :v${VERSION} || true

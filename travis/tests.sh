3f7c174c68d227c588e0b6db290a6739dbfbb40c
Bruno Dutra <brun_dutra@hotmail.com>
2019-03-22 01:06:17 -0300

    Adicionar teste e2e no Travis CI - 2

--- travis/tests.sh
+++ travis/tests.sh
@@ -3,5 +3,14 @@ set -e
 echo "BRANCH: $TRAVIS_BRANCH | TESTS - $VERSION"
 
 npm run lint
+
 npm run unit-ci
+
+webdriver-manager update --standalone false --gecko false --versions.chrome 2.35
 npm run e2e-ci
+
+sonar-scanner -Dsonar.projectVersion=${VERSION}          
+                  -Dsonar.links.homepage=https://github.com/${TRAVIS_REPO_SLUG}
+                  -Dsonar.links.ci=https://travis-ci.com/${TRAVIS_REPO_SLUG}
+                  -Dsonar.links.scm=https://github.com/${TRAVIS_REPO_SLUG}
+                  -Dsonar.links.issue=https://github.com/${TRAVIS_REPO_SLUG}/issues

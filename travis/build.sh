93bc49738f896abfec2705d7e2c6667c6f46f3c7
Bruno Dutra <brun_dutra@hotmail.com>
2019-03-23 17:38:40 -0300

    :running:

--- travis/build.sh
+++ travis/build.sh
@@ -7,3 +7,4 @@ echo "BRANCH: $TRAVIS_BRANCH | BUILD - $VERSION"
 
 ionic cordova build android --prod --release
 
+cp platforms/android/app/build/outputs/apk/release/app-release-unsigned.apk ~/shared/app-release-unsigned.apk
--- travis/sign.sh
+++ travis/sign.sh
@@ -14,10 +14,13 @@ sign_apk () {
     
     jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -storepass $KEYSTORE_PASSWORD -keystore /home/travis/build/$TRAVIS_REPO_SLUG/ionictravis.keystore $file ionic_travis_key
     
-    ${ANDROID_HOME}/build-tools/27.0.3/zipalign -v 4 $file app.apk
+    ${ANDROID_HOME}/build-tools/28.0.3/zipalign -v 4 $file app.apk
     
     rm $file
 
 }
 
-sign_apk app-release-unsigned.apk platforms/android/app/build/outputs/apk/release
+mkdir -p build
+mv ~/shared/app-release-unsigned.apk build/app-release-unsigned.apk
+sign_apk app-release-unsigned.apk build
+cp build/app.apk ~/shared/app.apk

ffef6ee908efbf3514e9177cebc991b608c9d9fd
Bruno Dutra <brun_dutra@hotmail.com>
2019-03-24 16:25:36 -0300

    Alterar URL do Cache OSX

--- travis/osx/before_install.sh
+++ travis/osx/before_install.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
 echo 'Fetching cache...'
-curl -O https://s3.amazonaws.com/ionic-travis/osx-cache/homebrew-cache.tar.gz
+        
+curl -O https://s3-sa-east-1.amazonaws.com/ionic-travis/osx-cache/homebrew-cache.tar.gz
 
 if [ -f homebrew-cache.tar.gz ]; then
   if ! tar tf homebrew-cache.tar.gz &>/dev/null; then

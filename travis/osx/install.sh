443e05f19714077e0716753a2da219ce6f8b5552
Bruno Dutra <brun_dutra@hotmail.com>
2019-03-26 08:48:10 -0300

    Remover node ao Homebrew

--- travis/osx/install.sh
+++ travis/osx/install.sh
@@ -5,6 +5,7 @@ echo 'Fetching cache...'
 curl -O https://s3-sa-east-1.amazonaws.com/ionic-travis/osx-cache/homebrew-cache.tar.gz
 
 node -v
+npm -v
 if [ -f homebrew-cache.tar.gz ]; then
   if ! tar tf homebrew-cache.tar.gz &>/dev/null; then
     echo 'Cache was bad or empty'
@@ -12,13 +13,13 @@ if [ -f homebrew-cache.tar.gz ]; then
     echo 'Installing dependencies...'
     brew update
     brew install cocoapods || brew link --overwrite cocoapods
-    brew install node@10 ios-sim jq ios-deploy  
+    brew install ios-sim jq ios-deploy  
   else
     echo 'Extracting dependencies...'
     tar xzf homebrew-cache.tar.gz -C /usr/local/Cellar
 
     echo 'Linking dependencies...'
-    brew link node@10 ios-sim jq ios-deploy
+    brew link ios-sim jq ios-deploy
     brew link --overwrite cocoapods
   fi
 fi
